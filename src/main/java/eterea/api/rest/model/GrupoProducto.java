26cf4c8a59eef6c0d02ab4a351d7ebfa5c36157b
dqmdz <daniel.quinterospinto@gmail.com>
2022-08-10 21:08:53 -0300

    Refactor Imports

--- src/main/java/eterea/api/rest/model/GrupoProducto.java
+++ src/main/java/eterea/api/rest/model/GrupoProducto.java
@@ -5,52 +5,44 @@
  */
 package eterea.api.rest.model;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
 /**
- *
  * @author Romina
  */
 @Data
 @Entity
-@Table(name = "grupoproducto", uniqueConstraints = { @UniqueConstraint(columnNames = { "grp_gru_id", "grp_prd_id" }) })
+@Table(name = "grupoproducto", uniqueConstraints = {@UniqueConstraint(columnNames = {"grp_gru_id", "grp_prd_id"})})
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
 public class GrupoProducto extends Auditable implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7629544838576508960L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7629544838576508960L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "grp_id")
-	private Long grupoproductoId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "grp_id")
+    private Long grupoproductoId;
 
-	@Column(name = "grp_gru_id")
-	private Integer grupoId;
+    @Column(name = "grp_gru_id")
+    private Integer grupoId;
 
-	@Column(name = "grp_prd_id")
-	private Integer productoId;
+    @Column(name = "grp_prd_id")
+    private Integer productoId;
 
-	@Column(name = "grp_coeficiente")
-	@NotNull
-	private BigDecimal coeficiente = new BigDecimal(0);
+    @Column(name = "grp_coeficiente")
+    @NotNull
+    private BigDecimal coeficiente = new BigDecimal(0);
 
 }
