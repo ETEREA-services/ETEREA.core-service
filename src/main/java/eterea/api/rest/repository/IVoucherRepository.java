de68fd3a621899cba3f8a41030467e7eae0439ac
dqmdz <daniel.quinterospinto@gmail.com>
2022-08-10 21:32:44 -0300

    Update IVoucherRepository.java
    Add Anotations Repository

--- src/main/java/eterea/api/rest/repository/IVoucherRepository.java
+++ src/main/java/eterea/api/rest/repository/IVoucherRepository.java
@@ -10,14 +10,18 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import eterea.api.rest.model.Voucher;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author daniel
  *
  */
+@Repository
 public interface IVoucherRepository extends JpaRepository<Voucher, Long>{
 	
 	public List<Voucher> findAllByFechaVencimientoAndUsuario(OffsetDateTime fechaVencimiento, String usuario);
 
 	public Optional<Voucher> findByReservaId(Long reservaId);
+
+	Optional<List<Voucher>> findAllByFechaServicio(OffsetDateTime fechaServicio);
 }
