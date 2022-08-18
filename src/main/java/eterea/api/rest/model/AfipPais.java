66d705480c7f28e84be0d64f55059ad5ea3ce7e9
dqmdz <daniel.quinterospinto@gmail.com>
2022-08-18 16:42:11 -0300

    Update AfipPais.java
    Se elimina el @Data y se reemplaza por sus equivalentes

--- src/main/java/eterea/api/rest/model/AfipPais.java
+++ src/main/java/eterea/api/rest/model/AfipPais.java
@@ -1,28 +1,25 @@
 /**
- * 
+ * Entidad AfipPais relacionada a la tabla paisafip que guarda
+ *
  */
 package eterea.api.rest.model;
 
-import java.io.Serializable;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-/**
- * @author alma
- *
- */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "paisafip")
-@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
 public class AfipPais extends Auditable implements Serializable {
@@ -41,4 +38,17 @@ public class AfipPais extends Auditable implements Serializable {
 	
 	@Column(name = "auto_id")
 	private Long autoId;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		AfipPais afipPais = (AfipPais) o;
+		return afipPaisId != null && Objects.equals(afipPaisId, afipPais.afipPaisId);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }
