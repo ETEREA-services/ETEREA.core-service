ac68a95afb8534c7dd3841c177922ee06ddf7dc9
dqmdz <daniel.quinterospinto@gmail.com>
2022-09-01 09:08:35 -0300

    Update ClienteInternet.java

--- src/main/java/eterea/api/rest/model/ClienteInternet.java
+++ src/main/java/eterea/api/rest/model/ClienteInternet.java
@@ -1,35 +1,32 @@
 /**
- * 
+ * Entidad de la tabla clienteinternet
+ * TO DO: el tipo de cliente deberia ser una atributo de la clase cliente,
+ * o refatorizar a un modelo de usuario de aplicativo web, como paso previo y una
+ * vez realice alguna compra pasar a ser cliente
  */
 package eterea.api.rest.model;
 
-import java.io.Serializable;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-/**
- * @author daniel
- *
- */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "clienteinternet")
-@EqualsAndHashCode(callSuper = false)
-@NoArgsConstructor
 @AllArgsConstructor
 public class ClienteInternet extends Auditable implements Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -7817956435234734227L;
 
 	@Id
@@ -40,4 +37,16 @@ public class ClienteInternet extends Auditable implements Serializable {
 	@Size(max = 64)
 	private String password;
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		ClienteInternet that = (ClienteInternet) o;
+		return clienteId != null && Objects.equals(clienteId, that.clienteId);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }
