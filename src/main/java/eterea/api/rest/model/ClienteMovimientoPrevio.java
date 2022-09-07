16d360821bc8341bcac20059cf4af9ddeda68565
dqmdz <daniel.quinterospinto@gmail.com>
2022-09-07 16:03:15 -0300

    Update ClienteMovimientoPrevio.java

--- src/main/java/eterea/api/rest/model/ClienteMovimientoPrevio.java
+++ src/main/java/eterea/api/rest/model/ClienteMovimientoPrevio.java
@@ -1,37 +1,35 @@
 /**
- * 
+ * Entidad de la tabla clientemovimientoprevio, al momento no posee registros
+ * se vizualiza en el digrama de ER como una tabla union entre clientes y movimientode cliente
+ * TODO: revisar el modelo y planter la funcionalidad de la entidad ya que si sera solo una tabla union
+ * TODO: ...debe ser resuelta mediante anotaciones en las claves, de las entidades que une
  */
 package eterea.api.rest.model;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-/**
- * @author alma
- *
- */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "clientemovimientoprevio")
-@EqualsAndHashCode(callSuper = false)
-@NoArgsConstructor
 @AllArgsConstructor
-public class ClienteMovimientoPrevio extends Auditable implements Serializable {/**
-	 * 
-	 */
+public class ClienteMovimientoPrevio extends Auditable implements Serializable {
+
+	@Serial
 	private static final long serialVersionUID = -4005223853384447248L;
 
 	@Id
@@ -63,4 +61,16 @@ public class ClienteMovimientoPrevio extends Auditable implements Serializable {
 	@Column(name = "clientemovimiento_id")
 	private Long clienteMovimientoId;
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		ClienteMovimientoPrevio that = (ClienteMovimientoPrevio) o;
+		return clienteMovimientoPrevioId != null && Objects.equals(clienteMovimientoPrevioId, that.clienteMovimientoPrevioId);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
 }
