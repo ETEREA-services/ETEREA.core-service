a00ae85aefe2819e886ee9626856a983711cfa0d
dqmdz <daniel.quinterospinto@gmail.com>
2024-01-30 17:38:57 -0300

    Agregado de reintento de facturación pendiente

--- src/main/java/eterea/core/api/rest/service/ReservaContextService.java
+++ src/main/java/eterea/core/api/rest/service/ReservaContextService.java
@@ -6,6 +6,8 @@ import eterea.core.api.rest.kotlin.repository.ReservaContextRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReservaContextService {
 
@@ -14,6 +16,10 @@ public class ReservaContextService {
     @Autowired
     public ReservaContextService(ReservaContextRepository repository) {
         this.repository = repository;
+    }
+
+    public List<ReservaContext> findAllByFacturacionPendiente() {
+        return repository.findAllByFacturaPendiente((byte) 1);
     }
 
     public ReservaContext findByVoucherId(Long voucherId) {
@@ -40,4 +46,5 @@ public class ReservaContextService {
             return repository.save(reservaContext);
         }).orElseThrow(() -> new ReservaContextException(reservaContextId));
     }
+
 }
