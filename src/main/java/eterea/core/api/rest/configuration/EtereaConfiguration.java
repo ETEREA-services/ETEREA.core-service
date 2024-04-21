ad161be6932a1690bf5610376048787e40df4d2a
dqmdz <daniel.quinterospinto@gmail.com>
2024-04-21 16:21:36 -0300

    First deploy

--- src/main/java/eterea/core/api/rest/configuration/EtereaConfiguration.java
+++ src/main/java/eterea/core/api/rest/configuration/EtereaConfiguration.java
@@ -3,6 +3,7 @@
  */
 package eterea.core.api.rest.configuration;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableJpaAuditing
 @EnableScheduling
+@EnableDiscoveryClient
 @PropertySource("classpath:config/eterea.properties")
 public class EtereaConfiguration {
 
