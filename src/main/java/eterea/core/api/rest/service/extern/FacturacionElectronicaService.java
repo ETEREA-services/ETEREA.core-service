e0ca1f3ecb2804044980efa95362f8e41e011dac
dqmdz <daniel.quinterospinto@gmail.com>
2024-08-31 21:14:34 -0300

    Upgrade versions and change pyafipws to eureka

--- src/main/java/eterea/core/api/rest/service/extern/FacturacionElectronicaService.java
+++ src/main/java/eterea/core/api/rest/service/extern/FacturacionElectronicaService.java
@@ -1,5 +1,6 @@
 package eterea.core.api.rest.service.extern;
 
+import eterea.core.api.rest.client.afip.FacturacionAfipClient;
 import eterea.core.api.rest.kotlin.model.Negocio;
 import eterea.core.api.rest.kotlin.model.dto.FacturacionDto;
 import eterea.core.api.rest.service.NegocioService;
@@ -14,38 +15,14 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class FacturacionElectronicaService {
 
-    private final NegocioService negocioService;
+    private final FacturacionAfipClient facturacionAfipClient;
 
-    public FacturacionElectronicaService(NegocioService negocioService) {
-        this.negocioService = negocioService;
+    public FacturacionElectronicaService(FacturacionAfipClient facturacionAfipClient) {
+        this.facturacionAfipClient = facturacionAfipClient;
     }
 
-    private String getUrl(Integer negocioId) {
-        Negocio negocio = negocioService.findByNegocioId(negocioId);
-        return "http://" + negocio.getFacturaServer() + ":" + negocio.getFacturaPort() + "/facturacionService";
-    }
-
-    public FacturacionDto makeFactura(FacturacionDto facturacionDTO, Integer negocioId) {
-        WebClient webClient = WebClient.create(getUrl(negocioId));
-        facturacionDTO = webClient.post()
-                .body(BodyInserters.fromValue(facturacionDTO))
-                .retrieve()
-                .onStatus(
-                        // Manejar códigos de error específicos si es necesario
-                        status -> status.is4xxClientError() || status.is5xxServerError(),
-                        response -> {
-                            // En caso de error, manejar el cuerpo de la respuesta
-                            Mono<String> errorMono = response.bodyToMono(String.class);
-                            return errorMono.flatMap(errorMessage -> {
-                                // Puedes imprimir el mensaje de error o manejarlo de otra manera
-                                log.debug(errorMessage);
-                                return Mono.error(new WebClientResponseException("Error en la respuesta", response.statusCode().value(), errorMessage, null, null, null));
-                            });
-                        }
-                )
-                .bodyToMono(FacturacionDto.class)
-                .block();
-        return facturacionDTO;
+    public FacturacionDto makeFactura(FacturacionDto facturacionDTO) {
+        return facturacionAfipClient.facturador(facturacionDTO);
     }
 
 }
