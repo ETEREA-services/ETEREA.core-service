56c87bf972eded3d48eb3cd6bede9ccf7a086f1c
Sebastian <sebamorici@hotmail.com>
2024-11-28 15:12:35 -0300

    Verificar contraseña correcta con AuthController

---
+++ src/main/java/eterea/core/service/service/AuthService.java
@@ -0,0 +1,22 @@
+package eterea.core.service.service;
+
+import eterea.core.service.exception.UsuarioException;
+import eterea.core.service.repository.UsuarioRepository;
+import eterea.core.service.kotlin.model.Usuario;
+
+public class AuthService {
+   private final UsuarioRepository usuarioRepository;
+
+   public AuthService(UsuarioRepository usuarioRepository) {
+      this.usuarioRepository = usuarioRepository;
+   }
+
+   public boolean login(String username, String password) throws UsuarioException {
+      Usuario usuario = usuarioRepository
+         .findById(username)
+         .orElseThrow(() -> new UsuarioException(username));
+      String passwordHash = usuarioRepository.findPasswordHash(password);
+
+      return usuario.getPassword().equals(passwordHash);
+   }
+}
