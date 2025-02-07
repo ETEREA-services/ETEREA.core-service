30442a725787f09779078d9f6567d28475df9ee1
Sebastian <sebamorici@hotmail.com>
2025-02-07 11:55:27 -0300

    Correccion VoucherDto

--- src/main/java/eterea/core/service/model/dto/programadia/mapper/VoucherToDtoMapper.java
+++ src/main/java/eterea/core/service/model/dto/programadia/mapper/VoucherToDtoMapper.java
@@ -9,21 +9,20 @@ import eterea.core.service.model.dto.programadia.VoucherDto;
 
 @Component
 public class VoucherToDtoMapper implements Function<Voucher, VoucherDto> {
-   
+
    @Override
    public VoucherDto apply(Voucher voucher) {
       return new VoucherDto(
-         voucher.getVoucherId(),
-         voucher.getNombrePax(),
-         voucher.getCantidadPax(),
-         voucher.getSubeEn(),
-         voucher.getObservaciones(),
-         voucher.getConfirmado() == 1,
-         voucher.getPagaCacheuta() == 1,
-         voucher.getContacto(),
-         voucher.getPaxsReales(),
-         voucher.getReservaId(),
-         voucher.getNumeroVoucher()
-      );
+            voucher.getVoucherId(),
+            voucher.getNombrePax(),
+            voucher.getCantidadPax(),
+            voucher.getSubeEn(),
+            voucher.getObservaciones(),
+            voucher.getConfirmado() == 1,
+            voucher.getPagaCacheuta() == 1,
+            voucher.getContacto(),
+            voucher.getPaxsReales(),
+            voucher.getReservaId(),
+            voucher.getNumeroVoucher());
    }
 }
