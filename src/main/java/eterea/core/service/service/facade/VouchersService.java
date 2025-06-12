b28e77dc221d1ef55e160483ee26b1b2adba98f6
Sebastian <sebamorici@hotmail.com>
2025-06-12 03:06:41 +0000

    Pongo la reserva verificada al crear un voucher por EtereaOnline

--- src/main/java/eterea/core/service/service/facade/VouchersService.java
+++ src/main/java/eterea/core/service/service/facade/VouchersService.java
@@ -677,6 +677,7 @@ public class VouchersService {
                         break;
                 }
             }
+            reserva.setVerificada((byte) 1);
             reserva = reservaService.update(reserva, reserva.getReservaId());
             savedVoucher.setReservaId(reserva.getReservaId());
             savedVoucher = voucherService.save(savedVoucher);
